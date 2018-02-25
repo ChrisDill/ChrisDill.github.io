1c219604f7fb6fb2d838b0f75a062e738d726b82
ChrisDill <chris.rj.dill@gmail.com>
2018-02-25 14:34:39 +0000

    res renamed to assets

--- effects.js
+++ effects.js
@@ -1,10 +1,10 @@
 // background slideshow
 $.backstretch([
-	"res/land1.jpg",
-	"res/land2.jpg",
-	"res/land3.jpg",
-	"res/space1.jpg",
-	"res/space3.jpg",
+	"assets/land1.jpg",
+	"assets/land2.jpg",
+	"assets/land3.jpg",
+	"assets/space1.jpg",
+	"assets/space3.jpg",
 	], {duration: 6000, fade:1500});
 
 // fade in main content
