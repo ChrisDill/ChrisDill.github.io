8703beb96305e9bcfdd9ba68ef8978e1e3fd2058
ChrisDill <chris.rj.dill@gmail.com>
2018-05-08 21:24:58 +0100

    Moved scripts + added analytics
    - Moved js scripts into js folder.
    - Added google analytics script

---
+++ js/cookies.js
@@ -0,0 +1,12 @@
+/* Cookies Notification */
+let getCookiesRead = localStorage.getItem('cookiesRead');
+if (getCookiesRead == 'confirmed') {
+  cookiesNotification.style.display = 'none';
+} else {
+  cookiesNotification.style.display = 'block';
+}
+
+cookiesClose.addEventListener('click', function() {
+  cookiesNotification.style.display = 'none';
+  localStorage.setItem('cookiesRead', 'confirmed');
+});
