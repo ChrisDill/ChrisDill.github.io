0cd1ab1e7d9661b2692bf17956774a8b2a6215ad
ChrisDill <chris.rj.dill@gmail.com>
2018-06-28 08:47:54 +0100

    Nav,layout change
    - Moved title under nav bar.
    - Removed old backgrounds, testing ideas for new ones.

--- js/effects.js
+++ js/effects.js
@@ -1,15 +1,11 @@
 // background slideshow
 $.backstretch([
-	"/assets/land1.jpg",
-	"/assets/land2.jpg",
-	"/assets/land3.jpg",
-	"/assets/land4.jpg",
-	], {duration: 6500, fade:1500});
+	"/assets/scifi1.jpg",
+	"/assets/scifi2.jpg",
+	"/assets/purple-space1.jpg"
+], {duration: 6500, fade: 1500});
 
 // fade in main content
-$(document).ready(function() {			
+$(document).ready(function() {	
 		$("main").hide().fadeIn(700);
-		$(".arta").hide().fadeIn(1200);
-		$(".artb").hide().fadeIn(1700);
-		$(".artc").hide().fadeIn(2200);
 });
