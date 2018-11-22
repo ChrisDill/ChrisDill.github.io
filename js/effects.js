1eed21e222534442798d8d38132a8d27e205c8f2
ChrisDill <chris.rj.dill@gmail.com>
2018-11-22 21:21:07 +0000

    Minor tweaks and some fixed
    - Testing ideas for default layout.
    - Using fork of backstretch that fixes bug with white screen on refresh.
    - Fixed bug with dropdown menu.

--- js/effects.js
+++ js/effects.js
@@ -1,10 +1,18 @@
 // background slideshow
-$("main").backstretch([
+$("#box").backstretch([
 	"/assets/landscape1.jpg",
 	"/assets/scifi2.jpg",	
 	"/assets/space10.png",
-], {duration: 8000, fade: 1500});
+], {duration: 8000, fade: 1500, fadeFirst:false});
 
-// $(window).on("backstretch.before", function (e, instance, index) {
-//     instance.options.fade = 1000;
-// });
+// close solution to footer at bottom issue
+$(document).ready(function() {
+//    var docHeight = $(window).height();
+//    var footerHeight = $("footer").height();
+//    var footerTop = $("footer").position().top + footerHeight;
+//
+//    if (footerTop < docHeight) {
+////        $("footer").css("margin-top", 10 + (docHeight - footerTop) + "px");
+//				$("footer").css("margin-top", (docHeight - footerTop) - 16 + "px");
+//		}
+});
